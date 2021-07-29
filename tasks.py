f3b5d12f522a0dd6b7fa777e76b6564e2092f5e3
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-07-29 13:57:45 -0600

    put server in tasks

--- tasks.py
+++ tasks.py
@@ -3,6 +3,8 @@ import sys
 from pathlib import Path
 import tarfile
 import shutil
+import re
+from livereload import Server
 
 from invoke import task
 
@@ -10,7 +12,7 @@ BRANCH = 'archive'
 CURRENT = 'F21'
 BUILD_DIR = Path('_build')
 FULL_DIR = BUILD_DIR / 'site'
-CUR_DIR = BUILD_DIR / 'html'
+CUR_DIR = BUILD_DIR / 'dirhtml'
 CUR_DEPLOY_DIR = FULL_DIR / CURRENT
 
 
@@ -19,14 +21,34 @@ def _msg(fmt, *args):
 
 
 @task
-def build(c, rebuild=False):
+def build(c, rebuild=False, builder='dirhtml'):
     """
     Build the current site.
     """
     if rebuild:
-        c.run('jb build --all .')
+        c.run(f'jb build --all --builder {builder} .')
     else:
-        c.run('jb build .')
+        c.run(f'jb build --builder {builder} .')
+
+
+@task
+def serve(c, full_site=False, port=8000):
+    def rebuild():
+        _msg('building and copying site')
+        build(c)
+        if full_site:
+            build_site(c)
+
+    _ignore_re = re.compile(r'^(\.[\\/])?_build[\\/]')
+
+    def ignore(p):
+        return _ignore_re.match(p)
+
+    rebuild()
+    srv_root = FULL_DIR if full_site else CUR_DIR
+    server = Server()
+    server.watch('.', rebuild, ignore=ignore)
+    server.serve(port=port, root=srv_root)
 
 
 @task
@@ -54,3 +76,9 @@ def copy_root(c):
 def build_site(c):
     _msg('copying current tree to site target')
     shutil.copytree(CUR_DIR, CUR_DEPLOY_DIR, dirs_exist_ok=True)
+
+
+@task
+def clean(c):
+    _msg('removing build directory')
+    shutil.rmtree(BUILD_DIR)
