1cd63efbd21a798a4bf608f70c0270a572c7d914
Michael Ekstrand <michaelekstrand@boisestate.edu>
2021-11-04 14:40:58 -0600

    try again but nope

--- extensions/media/nav.py
+++ extensions/media/nav.py
@@ -14,39 +14,39 @@ _asn_re = re.compile(r'^assignments/A\d+/index')
 def eomjify_toc(app: Sphinx, env: BuildEnvironment):
     _log.info('adding emoji to TOC')
 
-    # for name in env.titles:
-    #     _log.debug('checking document %s', name)
-    #     title = env.titles[name]
-    #     txt = title.astext()
-    #     new = None
-    #     if _week_re.match(name):
-    #         print('fixing week', name)
-    #         new = f'🧩 {txt}'
-    #     elif _asn_re.match(name):
-    #         new = f'📩 {txt}'
+    for name in env.titles:
+        _log.debug('checking document %s', name)
+        title = env.titles[name]
+        txt = title.astext()
+        new = None
+        if _week_re.match(name):
+            print('fixing week', name)
+            new = f'🧩 {txt}'
+        elif _asn_re.match(name):
+            new = f'📩 {txt}'
 
-    #     if new:
-    #         nt = nodes.title()
-    #         nt += nodes.Text(new)
-    #         env.titles[name] = nt
+        if new:
+            nt = nodes.title()
+            nt += nodes.Text(new)
+            env.titles[name] = nt
 
-    for tt in env.tocs['index'].traverse(addnodes.toctree):
-        entries = tt['entries']
-        for i in range(len(entries)):
-            title, path = entries[i]
-            print(path, str(title).encode('ascii', errors='replace'))
-            if title:
-                continue
+    # for tt in env.tocs['index'].traverse(addnodes.toctree):
+    #     entries = tt['entries']
+    #     for i in range(len(entries)):
+    #         title, path = entries[i]
+    #         print(path, str(title).encode('ascii', errors='replace'))
+    #         if title:
+    #             continue
 
-            _log.debug('checking document %s', path)
-            txt = env.titles[path].astext()
-            new = None
-            if _week_re.match(path):
-                print('fixing week', path)
-                new = f'🧩 {txt}'
-            elif _asn_re.match(path):
-                new = f'📩 {txt}'
+    #         _log.debug('checking document %s', path)
+    #         txt = env.titles[path].astext()
+    #         new = None
+    #         if _week_re.match(path):
+    #             print('fixing week', path)
+    #             new = f'🧩 {txt}'
+    #         elif _asn_re.match(path):
+    #             new = f'📩 {txt}'
 
-            if new:
-                entries[i] = (new, path)
+    #         if new:
+    #             entries[i] = (new, path)
 
